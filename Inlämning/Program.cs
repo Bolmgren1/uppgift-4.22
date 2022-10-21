04af72a32426969ead605b50b83f38e8c1eaf4bc
TheBlueSoul5188 <kebo05001@utb.vaxjo.se>
2022-10-21 10:20:56 +0200

    Add project files.

---
+++ Inlämning/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+
+namespace ____
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 10;
+            while (i < 31) 
+            {
+                Console.WriteLine(i);
+                i++;
+                
+
+            }
+            
+            int t = 200;
+            while (t > 179)
+            {
+                Console.WriteLine(t);
+                t--;
+                
+            }
+            
+            int p = 1000;
+            while (p < 1401)
+            {
+                Console.WriteLine(p);
+                p = p + 50;
+               
+            }
+           
+        }
+    }
+}
